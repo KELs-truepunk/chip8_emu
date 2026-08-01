d901b739088784325dbdbf67eb8bebe6105b5364
Admin <ivan.bogdanov271210@gmail.com>
2026-08-01 14:29:22 +0500

    make the SDL2 window

--- src/main.c
+++ src/main.c
@@ -1,3 +1,24 @@
-int main(){
+#include <SDL2/SDL.h>
+#include "chip8.h"
+#include <stdio.h>
+
+const static char* window_title = "CHIP8 Emulator";
+
+int main()
+{
+
+    // Инициалзация SDL2
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        fprintf(stderr, "SDL Init Error: %s\n", SDL_GetError());
+        return -1;
+    }
+    //создаем окно с положением в центре, с заголовком, без флагов, разрешение 64 на 32
+    SDL_Window* window = SDL_CreateWindow(
+        window_title, 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        SCREEN_WIDTH, SCREEN_HEIGHT, 0
+    );
 
 }
